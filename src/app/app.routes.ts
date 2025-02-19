dcbbee514c11d2b531de3d7032f30369632e8b5c
red <purred@naver.com>
2025-02-19 11:20:41 +0900

    Form 적용

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import {HomePageComponent} from './pages/home-page/home-page.component';
 import {NotFoundComponent} from './pages/common/not-found/not-found.component';
 import {RxjsHomePageComponent} from './pages/rxjs/rxjs-home-page/rxjs-home-page.component';
+import {FormHomeComponent} from './pages/form-page/form-home/form-home.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'rxjs', component: RxjsHomePageComponent },
+  { path: 'form', component: FormHomeComponent },
   { path: 'counter', loadChildren: () =>
       import('./pages/counter/counter.routes').then((mod) => mod.COUNTER_ROUTES), },
   { path: 'simple_task', loadChildren: () =>
--- src/app/pages/form-page/form-home/form-home.component.ts
+++ src/app/pages/form-page/form-home/form-home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {LoginComponent} from './login/login.component';
 
 @Component({
   selector: 'app-form-home',
-  imports: [],
+  imports: [
+    LoginComponent
+  ],
   templateUrl: './form-home.component.html',
+  standalone: true,
   styleUrl: './form-home.component.scss'
 })
 export class FormHomeComponent {
